ca570c8d8f1997f550cae62f5a061db0cf8781f2
CenturyGate <centurygate@163.com>
2018-01-27 22:37:42 +0800

    [MOD]1、修改了typeof (networkinfo['eth0.1'])的方式，正确写法为typeof (networkinfo['eth0.1']) != 'undefined'      2、修改index.ejs中引用的js和css文件。去除了对jquery-ui的使用，原因： jquery-ui 与 jquery-mobile冲突，两者一起使用的话会造成 单选框或者下拉框显示异常。      3、进度条目前调整为使用jQMProgressBar

--- routes/applyconfig.js
+++ routes/applyconfig.js
@@ -5,65 +5,66 @@ var router = express.Router();
 var process = require('child_process');
 var configpath = "/root/.homebridge/config.json"
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
+    console.log("Enter applyconfig.js")
     var result = {};
     result.status = 'ok';
-    //这边要在实际删除文件夹成功后再发送成功
-    var networkinfo = os.networkInterfaces();
-    if(typeof (networkinfo['eth0.1'] != 'undefined'))
-    {
-        for(var i = 0; i < networkinfo['eth0.1'].length;i++)
-        {
-            if(typeof(networkinfo['eth0.1'][i]['mac']) != 'undefined')
-            {
-                var username = networkinfo['eth0.1'][i]['mac'].toUpperCase()
-                var configobj = JSON.parse(fs.readFileSync(configpath));
-                configobj['bridge']['username'] = username;
-                configobj['bridge']['name'] = configobj['bridge']['name']+username;
-                fs.writeFileSync(configpath,JSON.stringify(configobj,null,4));
-                console.log(JSON.stringify(configobj,null,4));
-                break;
+    try {
+        //这边要在实际删除文件夹成功后再发送成功
+        var networkinfo = os.networkInterfaces();
+        if (typeof (networkinfo['eth0.1']) != 'undefined') {
+            for (var i = 0; i < networkinfo['eth0.1'].length; i++) {
+                if (typeof(networkinfo['eth0.1'][i]['mac']) != 'undefined') {
+                    var username = networkinfo['eth0.1'][i]['mac'].toUpperCase()
+                    var configobj = JSON.parse(fs.readFileSync(configpath));
+                    configobj['bridge']['username'] = username;
+                    configobj['bridge']['name'] = configobj['bridge']['name'] + username;
+                    fs.writeFileSync(configpath, JSON.stringify(configobj, null, 4));
+                    console.log(JSON.stringify(configobj, null, 4));
+                    break;
+                }
             }
         }
+
+        process.exec('rm -rf /root/.homebridge/persist /root/.homebridge/accessories ', function (error, stdout, stderr) {
+            if (error) {
+                // console.error(`exec error: ${error}`);
+                result.status = 'errapplyconfig';
+                res.end(JSON.stringify(result));
+                return;
+            }
+            else {
+                console.log("ApplyConfig->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
+                result.status = 'ok';
+                res.end(JSON.stringify(result));
+                // console.log(`stdout: ${stdout}`);
+                // console.log(`stderr: ${stderr}`);
+                process.exec('reboot -f', function (error, stdout, stderr) {
+                    if (error) {
+                        // console.error(`exec error: ${error}`);
+                    }
+                    else {
+
+                        // console.log(`stdout: ${stdout}`);
+                        // console.log(`stderr: ${stderr}`);
+                    }
+                });
+            }
+
+        });
     }
-
-    process.exec('rm -rf /root/.homebridge/persist /root/.homebridge/accessories ',function(error, stdout, stderr){
-        if (error) {
-            // console.error(`exec error: ${error}`);
-            result.status = 'errapplyconfig';
-            res.end(JSON.stringify(result));
-            return;
-        }
-        else
-        {
-            console.log("ApplyConfig->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
-            result.status = 'ok';
-            res.end(JSON.stringify(result));
-            // console.log(`stdout: ${stdout}`);
-            // console.log(`stderr: ${stderr}`);
-            process.exec('reboot -f',function(error, stdout, stderr){
-                if (error) {
-                    // console.error(`exec error: ${error}`);
-                }
-                else
-                {
-
-                    // console.log(`stdout: ${stdout}`);
-                    // console.log(`stderr: ${stderr}`);
-                }
-            });
-        }
-
-    });
-    // console.log("ApplyConfig->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
-    // result.status = 'ok';
-    // res.end(JSON.stringify(result));
+    catch (err) {
+        console.log("Error: " + err);
+        result.status = 'errapplyconfig';
+        res.end(JSON.stringify(result));
+        return;
+    }
 });
 
 //添加这个代码是为了解决 向 /save  url post 过后经过 save.js 的router.post('/',function(req,res,next){... res.redirect('/'); })处理过后 浏览器地址栏并没有显示为
 // http://localhost:3000 的样子,而是显示为了http://localhost:3000/save 的样子,因此 如果服务器重启后浏览器重新刷新则默认是 get http://localhost:3000/save, 而如果没有
 //如下的get处理 会导致浏览器找不到对应的资源,因此这里重新将http://localhost:3000/save 重定向到http://localhost:3000
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.redirect('/');
 });
 
